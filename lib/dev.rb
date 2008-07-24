02276d17aafddc5b1682b43e7d7e39b93fb62218
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2008-07-23 23:55:38 -0700

    fix dev.rb

--- lib/dev.rb
+++ lib/dev.rb
@@ -1,18 +1,21 @@
 module TwitterShoes
 ### SOME STUFF FOR LOCAL DEVELOPMENT!
 module Dev
-  def testing_ui?
-    # true
+  def self.extended(app)
+    if app.testing_ui?
+      def update_fixture_file(timeline)
+        File.open(timeline_fixture_path, "w+") { |f| f.puts timeline.to_yaml }
+      end
+      
+      def timeline_fixture_path
+        File.join Dir.pwd, "timeline.yml"
+      end
+    end
   end
   
-  if testing_ui?
-    def update_fixture_file(timeline)
-      File.open(timeline_fixture_path, "w+") { |f| f.puts timeline.to_yaml }
-    end
-    
-    def timeline_fixture_path
-      File.join Dir.pwd, "timeline.yml"
-    end
+  # return true for certain side effects.
+  def testing_ui?
+    # true
   end
 end
 end
