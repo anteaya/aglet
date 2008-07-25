b716815d45d27a54327aeacfdc22f41de7f0b827
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2008-07-25 16:30:11 -0700

    renamed to aglet, added some stuff for more creative presentation of status age that might in fact suck

---
+++ aglet.rb
@@ -0,0 +1,152 @@
+Shoes.setup do
+  gem "twitter"
+end
+
+%w(
+timeout
+twitter
+
+dev
+errors
+helpers
+).each { |x| require x }
+
+Shoes.app :title => "aglet", :width => 275, :height => 585, :resizable => false do
+  extend TwitterShoes::Dev, TwitterShoes::Errors, TwitterShoes::Helpers
+  
+  cred_path = File.expand_path "~/.twittershoes_cred"
+  
+  @twitter = ::Twitter::Base.new *File.readlines(cred_path).map(&:strip)
+  
+  @friends = twitter_api { @twitter.friends.map(&:name) }
+  
+  ###
+  
+  def load_timeline
+    @timeline = (
+      if testing_ui?
+        YAML.load_file(timeline_fixture_path)
+      else
+        load_timeline_from_api
+      end || []
+    ).first(10)
+  end
+  
+  def load_timeline_from_api
+    twitter_api { @twitter.timeline }
+  end
+  
+  def reload_timeline
+    info "reloading timeline"
+    load_timeline
+    @timeline_stack.clear { @timeline.any? ? populate_timeline : twitter_down! }
+  end
+  
+  def update_status
+    if testing_ui?
+      status = ::Twitter::Status.new do |s|
+        s.text = @status.text
+        s.user = @timeline.first.user
+        s.created_at = Time.new
+        s.id = @timeline.first.id.to_i + 1
+      end
+      
+      timeline = [status] + @timeline[0..-2]
+      update_fixture_file timeline
+    else
+      twitter_api { @twitter.update @status.text }
+    end
+    
+    reload_timeline
+    reset_status
+  end
+  
+  # Layout for timeline
+  def populate_timeline
+    @timeline.each do |status|
+      flow :margin => 0 do
+        zebra_stripe gray(0.9)
+        # time_ago_bg status.created_at
+        
+        stack :width => -(45 + gutter) do
+          para autolink(status.text), :size => 9, :margin => 5
+        end
+        
+        stack :width => 45 do
+          image status.user.profile_image_url,
+            :width => 45, :height => 45, :radius => 5, :margin => [5,5,5,3]
+          
+          with_options :size => 7, :align => "center", :margin => [0,0,0,2] do |menu|
+            menu.para link(time_ago(status.created_at), :click => "http://twitter.com/statuses/#{status.id}")
+            menu.para link("reply")  { @status.text += "@#{status.user.screen_name} " ; @status.focus }
+            menu.para link("direct") { @status.text += "d #{status.user.screen_name} "; @status.focus }
+          end
+        end
+      end
+    end
+  end
+  
+  def reset_status
+    @status.text = ""
+    @counter.text = ""
+    @status.focus
+  end
+  
+  ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+  
+  # if not File.exist?(cred_path)
+  #   name = ask "user name?"
+  #   pass = ask "password?"
+  #   alert "Thank you, this info is now stored at #{cred_path}"
+  #   File.open(cred_path, "w+") { |f| f.puts name, pass }
+  # end
+  
+  if testing_ui? and not File.exist?(timeline_fixture_path)
+    update_fixture_file load_timeline_from_api
+  end
+  
+  ###
+  
+  background white
+  
+  # Longer entries will be published in full but truncated for mobile devices.
+  recommended_status_length = 140
+  
+  flow :margin => [0,0,0,5] do
+    background black
+    
+    @status = edit_box :width => -(55 + gutter), :height => 35, :margin => [5,5,5,0] do |s|
+      if s.text[-1] == ?\n
+        @submit.click
+      else
+        @counter.text = (size = s.text.size).zero? ? "" : size
+        @counter.style :stroke => (s.text.size > recommended_status_length ? red : @counter_default_stroke)
+      end
+    end
+    
+    @submit = button "»", :margin => 0 do
+      update_status
+    end
+    
+    @counter_default_stroke = white
+    @counter = strong ""
+    para @counter, :size => 8, :margin => [0,8,0,0], :stroke => @counter_default_stroke
+  end
+  
+  @timeline_stack = stack :height => 500, :scroll => true
+  
+  stack :height => 43 do
+    background black
+    # para "©2008 ", link("GREATsethPECTATIONS", :click => "http://greatseth.com", :hover => false),
+    #       :stroke => white, :margin => [0,5,0,0], :align => "center"
+  end
+  
+  ###
+  
+  reload_timeline
+  reset_status
+  
+  every 60 do
+    reload_timeline
+  end
+end
--- helpers.rb
+++ helpers.rb
@@ -39,26 +39,65 @@ module Helpers
     
     case minutes
       when 0..1
-        return (minutes == 0) ? 'less than a minute' : '1 minute' unless include_seconds
+        return (minutes == 0) ? "< 1 min" : "1 min" unless include_seconds
         case seconds
-          when 0..4   then 'less than 5 seconds'
-          when 5..9   then 'less than 10 seconds'
-          when 10..19 then 'less than 20 seconds'
-          when 20..39 then 'half a minute'
-          when 40..59 then 'less than a minute'
-          else             '1 minute'
+          when 0..4   then '< 5s'
+          when 5..9   then '< 10s'
+          when 10..19 then '< 20s'
+          when 20..39 then '30s'
+          when 40..59 then '< 1 min'
+          else             '1 min'
         end
 
-      when 2..44           then "#{minutes} minutes"
-      when 45..89          then 'about 1 hour'
-      when 90..1439        then "about #{(minutes.to_f / 60.0).round} hours"
+      when 2..44           then "#{minutes} min"
+      when 45..89          then '1 hr'
+      when 90..1439        then "#{(minutes.to_f / 60.0).round} hrs"
       when 1440..2879      then '1 day'
       when 2880..43199     then "#{(minutes / 1440).round} days"
-      when 43200..86399    then 'about 1 month'
+      when 43200..86399    then '1 month'
       when 86400..525599   then "#{(minutes / 43200).round} months"
-      when 525600..1051199 then 'about 1 year'
-      else                      "over #{(minutes / 525600).round} years"
+      when 525600..1051199 then '1 yr'
+      else                      "over #{(minutes / 525600).round} yrs"
     end
+  end
+  
+  def time_ago_bg(from, to = Time.new, include_seconds = false)
+    from = Time.parse from
+    
+    distance = (to - from).abs
+    minutes  = (distance / 60).round
+    seconds  = distance.round
+    
+    sec = [0.99, 0.95, 0.9, 0.85].map { |x| gray x }
+    min = gray(0.7)
+    hrs = gray(0.5)
+    day = gray(0.3)
+    yrs = gray(0.1)
+    
+    bg = case minutes
+      when 0..1
+        return background(min) unless include_seconds
+        case seconds
+          when 0..4   then sec[0]
+          when 5..9   then sec[1]
+          when 10..19 then sec[2]
+          when 20..39 then sec[3]
+          when 40..59 then min
+          else             min
+        end
+      
+      when 2..44           then min
+      when 45..89          then hrs
+      when 90..1439        then hrs
+      when 1440..2879      then day
+      when 2880..43199     then day
+      when 43200..86399    then yrs # months
+      when 86400..525599   then yrs # months
+      when 525600..1051199 then yrs
+      else                      yrs
+    end
+    
+    background bg
   end
 end
 end
